6a41a75b1723e2008d89400108f419aa80ca40a5
jantielens <j@tlns.be>
2014-11-27 15:59:42 +0100

    added client

---
+++ RaspberryPiThermometer.Client/RaspberryPiThermometer.Client/SensorValue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaspberryPiThermometer.Client
+{
+    public class SensorValue
+    {
+        public string Id { get; set; }
+        public string SensorName { get; set; }
+        public double Value { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
