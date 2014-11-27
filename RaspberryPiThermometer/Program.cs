6b384eeeae46e38bd4ccfc57cc1463be7d3b164d
jantielens <j@tlns.be>
2014-11-27 15:57:58 +0100

    v1

---
+++ RaspberryPiThermometer/Program.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaspberryPiThermometer
+{
+    class Program
+    {
+        public static MobileServiceClient MobileService = new MobileServiceClient(
+            "https://cloudthermometer.azure-mobile.net/",
+            "CETpZxkiFLxhQCnZvXKkqgllxDpNew44"
+        );
+
+
+        static void Main(string[] args)
+        {
+            System.Timers.Timer t = new System.Timers.Timer(2000);
+            t.Elapsed += (s, e) =>
+            {
+                Console.Write("Sending ... ");
+                double temp = GetTemperature(0);
+                Console.Write(temp.ToString());
+
+                try
+                {
+                    MobileService.GetTable<SensorValue>().InsertAsync(
+                              new SensorValue()
+                                  {
+                                      SensorName = "Raspberry Pi",
+                                      Timestamp = DateTime.Now,
+                                      Value = temp
+                                  }
+                                );
+                    Console.WriteLine(" ... done!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+                t.Enabled = true;
+            };
+
+            t.AutoReset = false;
+            t.Start();
+            Console.ReadKey();
+            t.Stop();
+        }
+
+        static double GetTemperature(int sensorIndex)
+        {
+            DirectoryInfo devicesDir = new DirectoryInfo("/sys/bus/w1/devices");
+            var deviceDir = devicesDir.GetDirectories("28*")[sensorIndex];
+            using (TextReader reader = 
+                new System.IO.StreamReader(deviceDir.FullName + "/w1_slave"))
+            {
+                string w1slavetext = reader.ReadToEnd();
+
+                string temptext =
+                    w1slavetext.Split(new string[] { "t=" }, 
+                    StringSplitOptions.RemoveEmptyEntries)[1];
+
+                double temp = double.Parse(temptext) / 1000;
+                return temp;
+            }
+        }
+    }
+}
